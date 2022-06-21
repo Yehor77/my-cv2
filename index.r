ef2adc55086d8c95037fce849705e57c377a5591
Yehor77 <107910503+Yehor77@users.noreply.github.com>
2022-06-21 09:59:16 +0300

    Add initial CV website files

---
+++ index.r
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Yehor</title>
+</head>
+
+<body>
+  <img src="фото" alt="profile picture">
+  <h1>Angela </h1>
+  <p><i>Founder and CEO of the App Brewery</i></p>
+  <p>I am the showrunner.I love watching football</p>
+  <hr size="2" noshade>
+  <h3>Books</h3>
+  <ul>
+    <li>complete ios app development boot camp</li>
+    <li>the complete web development bootcamp</li>
+  </ul>
+  <h3>my hobbies</h3>
+  <ol>
+  <li> playing computer games</li>
+  <li> watching films</li>
+  <li> dancing</li>
+  </ol>
+    /<body>
+
+</html>
